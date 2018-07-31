0563d17d1c4c4179cbbdfe3e6fc463ea68f50c8f
israeldelmal <40775374+israeldelmal@users.noreply.github.com>
2018-07-31 16:54:02 -0600

    Savefruit | Final

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,25 @@ $('.item-menu').on('click', function(event) {
 		scrollTop: $(Item).offset().top
 	}, 1000);
 });
+
+$('body > button').on('click', function(event) {
+	event.preventDefault();
+	$('body > nav').toggleClass('active-nav');
+
+	$('body > button > span').toggleClass('active-span');
+
+	if ($(window).width() <= 440) {
+		$('.item-menu').on('click', function(event) {
+			event.preventDefault();
+			let Item = $(this).attr('href');
+
+			$('body > nav').removeClass('active-nav');
+
+			$('body > button > span').removeClass('active-span');
+			
+			$('body, html').stop(true, true).animate({
+				scrollTop: $(Item).offset().top
+			}, 1000);
+		});
+	}
+});
